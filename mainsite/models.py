b71fef2055000ccac3c79ae736329ef47e1ae1aa
A M Corvi <amcorvi@icloud.com>
2017-08-06 04:49:43 -0400

    - Modified: description field of link model to change to text field with   variable length  - Corrected: about page template to remove superfluous markup

--- mainsite/models.py
+++ mainsite/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Link(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(min_length=30)
     url = models.URLField(max_length=50)
-    description = models.CharField(max_length=300, blank=True)
+    description = models.TextField(blank=True)
     date = models.DateTimeField(blank=True, auto_now_add=True)
 
     def __str__(self):
